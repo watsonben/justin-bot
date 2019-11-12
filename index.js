f544104446d6519dafc77bc1681a028d6e4f5b1a
Ben Watson <ben.watson@ezyvet.com>
2019-11-13 11:48:06 +1300

    feat: Slackbot to mimic Justin with some default replies

---
+++ index.js
@@ -0,0 +1,31 @@
+const SlackBot = require('slackbots');
+
+const bot = new SlackBot({
+    token: process.env.SLACK_TOKEN,
+    name: 'Justin Hallier',
+});
+const icon = 'https://avatars.slack-edge.com/2019-11-13/819110663314_b43a046dc4004e284606_512.jpg'
+const replies = [
+    'Sorry, don\'t want to help right now.',
+    'Nope, I don\'t feel like it.',
+    'Ughhh what?',
+    'Ask Eric instead.',
+    'Justin can\'t come to the phone right now. Leave a message and he\'ll (probably never) get back to you.',
+    'Nah, that\'s it: I\'m going to Europe.',
+    'No',
+    'No',
+    'No'
+]
+
+bot.on('message', (e) => {
+    if (e.type === "desktop_notification" && e.subtitle.includes('(bot)') === false) {
+        const reply = replies[Math.floor(Math.random() * replies.length)];
+	if (e.subtitle.charAt(0) === '#') {
+            // Post to a channel
+            bot.postMessageToChannel(e.subtitle.slice(1), reply, { icon_url: icon });
+	} else {
+            // Post to a user
+	    bot.postMessageToUser(e.subtitle, reply, { icon_url: icon });
+	}
+    }
+});
