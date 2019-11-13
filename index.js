fba24075637852ae7747d6ac349b0672da421eac
Ben Watson <ben.watson@ezyvet.com>
2019-11-13 14:16:08 +1300

    chore: tidy up the code
    Make the code more readable
    Add comments
    Add console.log to see if I can remove the if statement

--- index.js
+++ index.js
@@ -1,32 +1,40 @@
 const SlackBot = require('slackbots');
 
-const bot = new SlackBot({
-    token: process.env.SLACK_TOKEN,
-    name: 'Justin Hallier',
-});
-const icon = 'https://avatars.slack-edge.com/2019-11-13/819110663314_b43a046dc4004e284606_512.jpg'
+const user_name = 'Justin Hallier';
+const icon = 'https://avatars.slack-edge.com/2019-11-13/819110663314_b43a046dc4004e284606_512.jpg';
 const replies = [
-    'Sorry, don\'t want to help right now.',
-    'Nope, I don\'t feel like it.',
-    'Ughhh what?',
-    'Ask Eric instead.',
-    'Justin can\'t come to the phone right now. Leave a message and he\'ll (probably never) get back to you.',
-    'Nah, that\'s it: I\'m going to Europe.',
-    'No',
-    'No',
-    'No'
+  'Sorry, don\'t want to help right now.',
+  'Nope, I don\'t feel like it.',
+  'Ughhh what?',
+  'Ask Eric instead.',
+  'Justin can\'t come to the phone right now. Leave a message and he\'ll (probably never) get back to you.',
+  'Nah, I\'ve had enough; I\'m going to Europe.',
+  'No',
+  'No',
+  'No',
 ]
 
-bot.on('message', (e) => {
-    if (e.type === "desktop_notification" && e.subtitle.includes('(bot)') === false) {
-        const reply = replies[Math.floor(Math.random() * replies.length)];
-	if (e.subtitle.charAt(0) === '#') {
-            // Post to a channel
-            bot.postMessageToChannel(e.subtitle.slice(1), reply, { icon_url: icon });
-	} else {
-            // Post to a user
-	    console.log(e);
-	    bot.postMessage(e.channel, reply, { icon_url: icon });
-	}
-    }
+const bot = new SlackBot({
+  token: process.env.SLACK_TOKEN,
+  name: user_name,
 });
+
+bot.on('message', (event) => {
+  if ( // If someone sent us a message or tagged us, and it wasn't a bot.
+    event.type === 'desktop_notification'
+    && !event.subtitle.includes('(bot)')
+  ) {
+    const random_index = Math.floor(Math.random() * replies.length);
+    const reply = replies[random_index];
+
+    if (event.subtitle.charAt(0) === '#') {
+      // Post to a channel
+      console.log(event);
+      bot.postMessageToChannel(event.subtitle.slice(1), reply, { icon_url: icon })
+    }
+    else {
+      // Post to a user
+      bot.postMessage(event.channel, reply, { icon_url: icon })
+    }
+  }
+})
